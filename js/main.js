2337129bc052ea3e6204a73da04155b90b22d3fd
Arlez <dbarrera.ap@gmail.com>
2018-10-05 16:53:42 -0300

    pag up

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var btn = document.getElementById("boton");
+
+btn.addEventListener("click",function(){
+    alert("Para actvar esta función, contacte al desarrollador");
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
